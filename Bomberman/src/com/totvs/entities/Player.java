0a301539bd2f67a4ec073a4c00c23dea1450f9bb
Brenno10 <brenno10000@outlook.com>
2023-01-20 18:28:41 -0300

    O jogador agora pode pegar outro jogador

--- Bomberman/src/com/totvs/entities/Player.java
+++ Bomberman/src/com/totvs/entities/Player.java
@@ -1,6 +1,5 @@
 package com.totvs.entities;
 
-import com.totvs.graphics.Spritesheet;
 import com.totvs.main.Game;
 import com.totvs.world.World;
 
@@ -12,17 +11,16 @@ public class Player extends Entity {
     public final int downDir = 0, leftDir = 1, upDir = 2, rightDir = 3;
     public int dir = 0;
     public double speed = 2;
-    public int maxBombsAmount = 3;
-    public int bombPower = 1;
-    public int placedBombs = 0;
+    public boolean isTagged = false, isTouching = false;
 
     private int frames = 0, index;
     private final int maxFrames = 6, maxIndex = 2;
-    private boolean moved = false;
-    private BufferedImage[] rightPlayer;
-    private BufferedImage[] leftPlayer;
-    private BufferedImage[] upPlayer;
-    private BufferedImage[] downPlayer;
+    private boolean moved = false, canMove = true;
+
+    private final BufferedImage[] rightPlayer;
+    private final BufferedImage[] leftPlayer;
+    private final BufferedImage[] upPlayer;
+    private final BufferedImage[] downPlayer;
 
     public Player(int x, int y, int width, int height, BufferedImage sprite) {
         super(x, y, width, height, sprite);
@@ -32,47 +30,64 @@ public class Player extends Entity {
         upPlayer = new BufferedImage[3];
         downPlayer = new BufferedImage[3];
 
-        rightPlayer[0] = Game.player1Spritesheet.getSprite(0, 37, 16, 26);
-        rightPlayer[1] = Game.player1Spritesheet.getSprite(16, 38, 16, 26);
-        rightPlayer[2] = Game.player1Spritesheet.getSprite(33, 39, 16, 26);
+        for (int i = 0; i < rightPlayer.length; i++) {
+            rightPlayer[i] = sprite.getSubimage(i * 16, 39, 16, 26);
+            leftPlayer[i] = sprite.getSubimage(i * 16, 103, 16, 26);
+            upPlayer[i] = sprite.getSubimage(i * 16, 7, 16, 26);
+            downPlayer[i] = sprite.getSubimage(i * 16, 69, 16, 26);
+        }
+    }
 
-        leftPlayer[0] = Game.player1Spritesheet.getSprite(0, 101, 16, 26);
-        leftPlayer[1] = Game.player1Spritesheet.getSprite(16, 103, 16, 26);
-        leftPlayer[2] = Game.player1Spritesheet.getSprite(32, 102, 16, 26);
-
-        upPlayer[0] = Game.player1Spritesheet.getSprite(0, 7, 16, 26);
-        upPlayer[1] = Game.player1Spritesheet.getSprite(16, 7, 16, 26);
-        upPlayer[2] = Game.player1Spritesheet.getSprite(32, 7, 16, 26);
-
-        downPlayer[0] = Game.player1Spritesheet.getSprite(0, 69, 16, 26);
-        downPlayer[1] = Game.player1Spritesheet.getSprite(16, 69, 16, 26);
-        downPlayer[2] = Game.player1Spritesheet.getSprite(32, 69, 16, 26);
+    @Override
+    public void checkCollision() {
+        for (int i = 0; i < Game.players.size(); i++) {
+            if (!Game.players.get(i).equals(this)) {
+                isTouching = this.getHitBox().intersects(Game.players.get(i).getHitBox()) && this.isTagged;
+            }
+        }
     }
 
     @Override
     public void tick() {
+        this.updateHitbox(-1,  -3);
         moved = false;
-        if (right && World.isFree((int) (this.getX() + speed), this.getY())) {
+
+        if (right && World.isFree((int) (this.getX() + speed), this.getY()) && this.canMove) {
             moved = true;
             dir = rightDir;
             x += speed;
-        } else if (left && World.isFree((int) (this.getX() - speed), this.getY())) {
+        } else if (left && World.isFree((int) (this.getX() - speed), this.getY()) && this.canMove) {
             moved = true;
             dir = leftDir;
             x -= speed - 0.7;
         }
 
-        if (up && World.isFree(this.getX(), (int) (this.getY() - speed))) {
+        if (up && World.isFree(this.getX(), (int) (this.getY() - speed)) && this.canMove) {
             moved = true;
             dir = upDir;
             y -= speed - 0.7;
-        } else if (down && World.isFree(this.getX(), (int) (this.getY() + speed))) {
+        } else if (down && World.isFree(this.getX(), (int) (this.getY() + speed)) && this.canMove) {
             moved = true;
             dir = downDir;
             y += speed;
         }
 
-        // animação do jogador
+        // verifica se pegou o outro jogador
+        if (isTagged && !Game.tagIsOnCooldown) {
+            checkCollision();
+            for (int i = 0; i < Game.players.size(); i++) {
+                if (!Game.players.get(i).equals(this)) {
+                    if (isTouching) {
+                        Game.tagIsOnCooldown = true;
+                        Game.players.get(i).isTagged = true;
+                        this.isTagged = false;
+                        System.out.println("peguei vc " + (i + 1));
+                    }
+                }
+            }
+        }
+
+        // animação de movimento do jogador
         if (moved) {
             frames++;
             if (frames == maxFrames) {
@@ -82,8 +97,7 @@ public class Player extends Entity {
                     index = 0;
                 }
             }
-        }
-        else {
+        } else {
             index = 0;
         }
     }
