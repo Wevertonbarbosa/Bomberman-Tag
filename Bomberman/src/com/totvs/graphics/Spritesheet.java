ae5465cf3589cb7650d2f7d86227a4d53c6773b4
Brenno10 <brenno10000@outlook.com>
2023-01-20 18:29:44 -0300

    Adicionado método getSpritesheet

--- Bomberman/src/com/totvs/graphics/Spritesheet.java
+++ Bomberman/src/com/totvs/graphics/Spritesheet.java
@@ -4,13 +4,14 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Spritesheet {
     private BufferedImage spritesheet;
 
     public Spritesheet(String path) {
         try {
-            spritesheet = ImageIO.read(getClass().getResource(path));
+            spritesheet = ImageIO.read(Objects.requireNonNull(getClass().getResource(path)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -18,5 +19,9 @@ public class Spritesheet {
 
     public BufferedImage getSprite(int x, int y, int width, int height) {
         return spritesheet.getSubimage(x, y, width, height);
+    }
+
+    public BufferedImage getSpritesheet() {
+        return spritesheet;
     }
 }
