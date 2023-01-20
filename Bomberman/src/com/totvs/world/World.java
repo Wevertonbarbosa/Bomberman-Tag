48af87851fa515d063ed2c807a26f0b26c50c1ff
Brenno10 <brenno10000@outlook.com>
2023-01-20 18:19:39 -0300

    O criador de mapas foi recolocado

--- Bomberman/src/com/totvs/world/World.java
+++ Bomberman/src/com/totvs/world/World.java
@@ -3,9 +3,13 @@ package com.totvs.world;
 import com.totvs.entities.Player;
 import com.totvs.main.Game;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class World {
     public static Tile[] tiles;
@@ -13,17 +17,45 @@ public class World {
     public static final int TILE_SIZE = 16;
     public static List<Player> players = new ArrayList<>();
     public static int playerCount = 0;
+    public BufferedImage map;
 
-    public World() {
-        tiles = new Tile[WIDTH * HEIGHT];
+    public World(String path) {
+        try {
+            map = ImageIO.read(Objects.requireNonNull(getClass().getResource(path)));
+            players.add(Game.player1);
+            players.add(Game.player2);
 
-        players.add(Game.player1);
-        players.add(Game.player2);
+            int[] pixels = new int[map.getWidth() * map.getHeight()];
+            tiles = new Tile[map.getWidth() * map.getHeight()];
+            WIDTH = map.getWidth();
+            HEIGHT = map.getHeight();
 
-        for (int xx = 0; xx < WIDTH; xx++) {
-            for (int yy = 0; yy < HEIGHT; yy++) {
-                tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+            map.getRGB(0, 0, map.getWidth(), map.getHeight(), pixels, 0, map.getWidth());
+
+            // cria o mapa e coloca o jogador nele usando pixels coloridos (preto, branco, vermelho, azul)
+            for (int xx = 0; xx < map.getWidth(); xx++) {
+                for (int yy = 0; yy < map.getHeight(); yy++) {
+                    int pixelAtual = pixels[xx + (yy * map.getWidth())];
+
+                    if (pixelAtual == 0xff000000) {
+                        // chão / preto
+                        tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+                    } else if (pixelAtual == 0xffffffff) {
+                        // parede / branco
+                        tiles[xx + (yy * WIDTH)] = new WallTile(xx * 16, yy * 16, Tile.TILE_WALL);
+                    } else if (pixelAtual == 0xff0000ff && players.size() > playerCount) {
+                        // jogador / azul
+                        tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+                        players.get(playerCount).setX(xx * 16);
+                        players.get(playerCount).setY(yy * 16);
+                        playerCount++;
+                    } else {
+                        tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+                    }
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
