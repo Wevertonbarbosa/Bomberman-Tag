a5fc669dda8521ffb08a1ec11ff1b8116c8d2532
Brenno10 <brenno10000@outlook.com>
2023-01-20 18:23:48 -0300

    O jogo agora tem um tempo limite e "pegar" outro jogador tem cooldown

--- Bomberman/src/com/totvs/main/Game.java
+++ Bomberman/src/com/totvs/main/Game.java
@@ -13,46 +13,62 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Game extends Canvas implements Runnable, KeyListener {
-    private static JFrame frame;
+    public static JFrame frame;
+    private static final Random rand = new Random();
     private Thread thread;
     private boolean isRunning = true;
     private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private final int WIDTH = (int) screenSize.getWidth(), HEIGHT = (int) screenSize.getHeight();
     public static final int SCALE = 2;
+    public static boolean tagIsOnCooldown = false;
+    private int timer = 0, countDown = 0;
+    private final int gameDuration = (60 * 120);
 
     private final BufferedImage image;
 
     public static List<Entity> entities;
-    public static Spritesheet player1Spritesheet, tilesSpritesheet;
+    public static Spritesheet player1Spritesheet, player2Spritesheet, tilesSpritesheet;
 
     public World world;
 
+    public static List<Player> players = new ArrayList<>();
     public static Player player1;
     public static Player player2;
 
     public Game() {
-        addKeyListener(this);
-        initFrame();
-
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         entities = new ArrayList<>();
 
         // iniciando spritesheets
         tilesSpritesheet = new Spritesheet("/tiles_spritesheet.png");
         player1Spritesheet = new Spritesheet("/player1_spritesheet.png");
+        player2Spritesheet = new Spritesheet("/player2_spritesheet.png");
 
         // iniciando entidades
-        player1 = new Player(0 , 0, 8, 8,
-                player1Spritesheet.getSprite(0, 69, 16, 26));
+        player1 = new Player(0 , 0, 16, 16,
+                player1Spritesheet.getSprite(0, 0,
+                        player1Spritesheet.getSpritesheet().getWidth(),
+                        player1Spritesheet.getSpritesheet().getHeight()));
         entities.add(player1);
+        players.add(player1);
 
-        player2 = new Player((World.WIDTH * 16) - 16 , (World.HEIGHT * 16) - 16, 8, 8,
-                player1Spritesheet.getSprite(0, 69, 16, 26));
+        player2 = new Player((World.WIDTH * 16) - 16 , (World.HEIGHT * 16) - 16, 16, 16,
+                player2Spritesheet.getSprite(0, 0,
+                        player2Spritesheet.getSpritesheet().getWidth(),
+                        player2Spritesheet.getSpritesheet().getHeight()));
         entities.add(player2);
+        players.add(player2);
 
-        world = new World();
+        int playerIndex = rand.nextInt(0,players.size());
+        players.get(playerIndex).isTagged = true;
+        System.out.println("Ta com o Jogador " + (playerIndex + 1));
+
+        world = new World("/test_map.png");
+        addKeyListener(this);
+        initFrame();
     }
 
     // inicia a janela
@@ -60,7 +76,8 @@ public class Game extends Canvas implements Runnable, KeyListener {
         frame = new JFrame("Bomberman");
         frame.add(this);
         frame.setResizable(false);
-        frame.setSize(1000, 700);
+        frame.setSize(((world.map.getWidth() * 16) * SCALE) + 16,
+                ((world.map.getHeight() * 16) * SCALE) + 39);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -87,8 +104,27 @@ public class Game extends Canvas implements Runnable, KeyListener {
 
     // Roda a cada frame
     public void tick() {
+        countDown++;
+        if (countDown == gameDuration) {
+            for (int i = 0; i < players.size(); i++) {
+                if (!players.get(i).isTagged) {
+                    System.out.println("O vencedor é o jogador " + (i + 1));
+                }
+            }
+            isRunning = false;
+            return;
+        }
+
         for (int i = 0; i < entities.size(); i++) {
             entities.get(i).tick();
+        }
+
+        if (tagIsOnCooldown) {
+            timer++;
+            if (timer == 30) {
+                timer = 0;
+                tagIsOnCooldown =  false;
+            }
         }
     }
 
